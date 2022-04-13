0525c3723ce7a61fe5d07665bd6aad4e339a3ca0
Mehmet Tarhan <hi@memtarhan.com>
2022-04-13 17:22:41 +0100

    [Working with the Combine Framework] - Using .combineLatests, .zip and .merge

--- 13-working-with-the-combine-framework/MyPlayground.playground/Contents.swift
+++ 13-working-with-the-combine-framework/MyPlayground.playground/Contents.swift
@@ -58,3 +58,43 @@ print("*******************")
 let scanExample = [1, 2, 3, 4].publisher
     .scan(1, { $0 * $1 })
     .sink(receiveValue: { print("\($0)", terminator: " ") })
+
+print("*******************")
+
+// Using combineLatest
+/// It is a publisher that combines the latest values from two other publishers. Both publishers must have the same failure type. The downstream subscriber will receive a tuple of the most recent elements from the upstream publishers when any of them emit a new value.
+
+let chars = PassthroughSubject<String, Never>()
+let numbers = PassthroughSubject<Int, Never>()
+let cancellableCombineLatest = chars.combineLatest(numbers)
+    .sink { print("Combine Latest Result: \($0).") }
+
+print("*******************")
+
+// Using merge
+/// With merge, we will aggregate multiple input publishers into a single stream, and the output will be just the latest value from any of them
+
+let oddNumbers = PassthroughSubject<Int, Never>()
+let evenNumbers = PassthroughSubject<Int, Never>()
+let cancellable2 = oddNumbers.merge(with: evenNumbers)
+    .sink { print("Result: \($0).") }
+oddNumbers.send(1)
+evenNumbers.send(2)
+oddNumbers.send(3)
+
+print("*******************")
+
+// Using zip
+/// zip is a publisher that emits a pair of elements when both input publishers have emitted a new value
+let cancellableZip = chars.zip(numbers)
+    .sink { print("Zip Result: \($0).") }
+
+chars.send("a")
+numbers.send(1)
+chars.send("b")
+chars.send("c")
+numbers.send(2)
+numbers.send(3)
+/*
+ Notice how zip doesn't send a new pair of values downstream until both of the publishers have emitted a new value. And when that happens, it will send a tuple with the oldest non-emitted values of both of them. CombineLatest will use the most recent and will emit a tuple every time one of the publishers emits a single new value (it will not wait to have both publishers emitting!). That is the main difference.
+ */
